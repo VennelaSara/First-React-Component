83d904ba54e4d41326291bcc23751362977c9329
VennelaSara <168427464+VennelaSara@users.noreply.github.com>
2024-11-04 19:52:34 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import KgButton from "./KgButton";
+
+function App(){
+return <div><h1>
+  This is the Best Course!!!
+</h1><button>Subscribe.</button>
+<KgButton></KgButton>
+</div>
+}
+
+export default App;
---
+++ Hello.jsx
@@ -0,0 +1,14 @@
+function Hello(){
+var myGoal = "React";
+let fullname = () =>
+{
+  return "Sara Vennela";
+}
+return <h3>
+
+  Hello this is the Future Speaking .I am Your Master {fullName()} and today i want to Complete {myGoal}
+  <p>{Math.round(Math.random())}</p>
+</h3>
+
+}
+export default Hello;
